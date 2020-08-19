6e6a081c92e6a6e393835dd44fbf14072a17d99b
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-08-19 10:51:32 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+
+katz_deli=[]
+
+def take_a_number(katz_deli,name)
+    katz_deli.push(name)
+    index=katz_deli.index(name) + 1
+    puts "Welcome, #{name}. You are number #{index} in line."
+end
