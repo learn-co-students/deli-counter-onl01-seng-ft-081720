af620f2db3a27ba5f754142ea8635ec58e837708
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-08-19 10:57:53 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,20 @@
 
 katz_deli=[]
 
+def line(katz_deli)
+    if katz_deli.size == 0
+        puts "The line is currently empty."
+    end
+    string= "The line is currently: "
+
+    katz_deli.each_with_index do |i,j|
+        string += "#{j+1}. #{i}"
+    end
+
+    puts string
+
+end
+
 def take_a_number(katz_deli,name)
     katz_deli.push(name)
     index=katz_deli.index(name) + 1
