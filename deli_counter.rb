e314ff8ca659789c4738b5b35774f9cc7f78b78c
ts-miller <tyler@DESKTOP-RNOETJO.localdomain>
2020-08-19 23:10:54 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+katz_deli = []
+
+def take_a_number(deli, name)
+    puts "Welcome, #{name}. You are number #{deli.length + 1} in line."
+    deli << name
+end
+
+def line(deli)
+    if deli.length == 0
+        puts "The line is currently empty."
+    else
+        position = []
+        deli.each.with_index(1) do |name, i|
+            position << " #{i}. #{name}"
+        end
+        puts "The line is currently:#{position.join}"
+    end
+end
+
+def now_serving(deli)
+    if deli.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{deli.shift}."
+    end
+end
